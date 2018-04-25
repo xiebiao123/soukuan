0d3bf440f6098acc477874b468991b6045f4349d
xiebiao <xiebiao@wshifu.com>
2018-04-25 21:51:23 +0800

    xxx2018/04/25

---
+++ soukuan-good-service/src/main/java/com/soukuan/GoodServiceApplication.java
@@ -0,0 +1,26 @@
+package com.soukuan;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@SpringBootApplication
+@MapperScan("com.soukuan.mapper")
+@EnableTransactionManagement
+@EnableDiscoveryClient
+public class GoodServiceApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(
+            SpringApplicationBuilder application) {
+        return application.sources(GoodServiceApplication.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoodServiceApplication.class, args);
+    }
+}
