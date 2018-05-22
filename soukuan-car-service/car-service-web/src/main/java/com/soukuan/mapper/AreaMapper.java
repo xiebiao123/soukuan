20946057b393e7725910b7b670da03ba8148a2a0
xiebiao <xiebiao@wshifu.com>
2018-05-22 15:47:49 +0800

    xxxxx

---
+++ soukuan-car-service/car-service-web/src/main/java/com/soukuan/mapper/AreaMapper.java
@@ -0,0 +1,8 @@
+package com.soukuan.mapper;
+
+
+import com.soukuan.common.car.domain.Area;
+import com.soukuan.web.templ.IBaseCommMapper;
+
+public interface AreaMapper extends IBaseCommMapper<Area> {
+}
